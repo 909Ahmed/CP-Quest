3d916ac58bd0c2404699d0d95231827042c43def
909hehe <140858364+909hehe@users.noreply.github.com>
2023-08-13 16:31:54 +0530

    who?

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,7 +1,7 @@
 var jwt = require('jsonwebtoken');
 const JWT_SECRET = 'ASecretKey';
 
-const fetchUser = (req ,res ,next)=>{
+const fetchUser = (req ,res ,next)=>{ 
     const token = req.header('auth-token');
     if(!token){
         res.status(401).send({error : "Please valid token"})
